07e4e7ed2ce64484da9b6f0bf7b9bdc86024254c
Blake Contreras <shardsofsilver@gmail.com>
2016-07-28 13:32:47 -0700

    Add unit tests for cards

---
+++ tests/unit/models/card-test.js
@@ -0,0 +1,28 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('card', 'Unit | Model | card', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('it exists', function(assert) {
+  let model = this.subject();
+  // let store = this.store();
+  assert.ok(!!model);
+});
+
+test('it has a name', function(assert) {
+
+});
+
+test('it has a cost', function(assert) {
+
+});
+
+test('it has an effect', function(assert) {
+
+});
+
+test('it has a graphic', function(assert) {
+
+});
