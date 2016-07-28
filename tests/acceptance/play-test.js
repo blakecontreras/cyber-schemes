0e57f25bdc4ba12522ae6152090b1b88a4e1ac0e
Blake Contreras <shardsofsilver@gmail.com>
2016-07-28 13:17:48 -0700

    Add acceptance tests for /play route.

--- tests/acceptance/play-test.js
+++ tests/acceptance/play-test.js
@@ -1,12 +1,49 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'cyber-schemes/tests/helpers/module-for-acceptance';
 
-moduleForAcceptance('Acceptance | play field');
+moduleForAcceptance('Acceptance | play');
 
 test('visiting /play', function(assert) {
   visit('/play');
 
   andThen(function() {
     assert.equal(currentURL(), '/play');
+  });
+});
+
+test('play a card', function(assert) {
+  visit('/play');
+  click('div:contains("Card")');
+
+  andThen(function() {
+    assert.ok(find('div.field.card'));
+  });
+});
+
+test('summon a hero', function(assert) {
+  visit('/play');
+  click('div:contains("Hero")');
+
+  andThen(function() {
+    assert.ok(find('div.field.hero'));
+  });
+});
+
+test('draw a scheme card', function(assert) {
+  visit('/play');
+  click('div:contains("Deck")');
+
+  andThen(function() {
+    assert.ok(find('div.field.scheme'));
+  });
+});
+
+test('reset the game', function(assert) {
+  visit('/play');
+  click('div:contains("Card")');
+  click('button:contains("Reset")');
+
+  andThen(function() {
+    assert.notOk(find('div.field.card'));
   });
 });
